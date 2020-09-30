c3f881bc90cc258add3e9c433c00a5d0d4d39bb3
Karol Krokowski <krokowskikarol1990@gmail.com>
2020-09-30 09:03:42 +0200

    added VL,SP and IP slats

--- src/containers/App.js
+++ src/containers/App.js
@@ -21,8 +21,7 @@ class App extends Component {
 
   //create json from files from file nemes
   createJSONFromFileNames = () => {
-    const list =
-      "MDS001.png    MDS040.png   MDS110.png   MDS2013.png  MDS2420.png  MDS810.png    MDS010.png    MDS050.png   MDS111.png   MDS2020.png  MDS2430.png  MDS811.png    MDS011.png    MDS051.png   MDS113.png   MDS210.png   MDS2440.png  MDS812.png    MDS012.png    MDS053P.png  MDS114.png   MDS211.png   MDS310.png   MDS851.png    MDS013.png    MDS054N.png  MDS122.png   MDS212.png   MDS315.png   MDS854.png    MDS014.png    MDS060.png   MDS2001.png  MDS213.png   MDS320.png   MDS855.png    MDS015.png    MDS061.png   MDS2002.png  MDS222.png   MDS400N.png    MDS020.png    MDS062.png   MDS2003.png  MDS2300.png  MDS801.png    MDS021PL.png  MDS063.png   MDS2010.png  MDS2310.png  MDS802.png  MDS022.png    MDS064.png   MDS2011.png  MDS2400.png  MDS803.png    MDS030.png    MDS101.png   MDS2012.png  MDS2410.png  MDS804.png"; // add file name from >>cmd ls
+    const list = "";
     const parsed = list
       .split(" ")
       .filter((item) => {
@@ -65,7 +64,7 @@ class App extends Component {
   };
   render() {
     //console.log("aaaaaa");
-    // console.log(this.createJSONFromFileNames());
+    //console.log(this.createJSONFromFileNames());
 
     // if (this.state.data.length === 0) {
     //   this.setState({ data: Data });
@@ -84,7 +83,7 @@ class App extends Component {
           <p>
             Jeżeli poszukiwanym elementem jest połowka np: <b>XIP010A</b> nalezy
             podać indeks pełnego profilu tj. <b>IP010</b> lub np. <b>IP030</b> a
-            nastepnie sprawdzić oznaczenia połowek.
+            nastepnie sprawdzić oznaczenia połówek.
           </p>
         </section>
       </div>
