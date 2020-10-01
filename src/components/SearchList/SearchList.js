1af838cd8cd4e1747da963171035b4d0c3d0ab22
Karol Krokowski <krokowskikarol1990@gmail.com>
2020-10-01 07:26:05 +0200

    added image folder / 150 / containing small jpg files for use in SearchResult as icons

--- src/components/SearchList/SearchList.js
+++ src/components/SearchList/SearchList.js
@@ -2,24 +2,29 @@ import React from "react";
 import SearchResult from "./SearchResult/SearchResult";
 import "./SearchList.css";
 const searchList = (props) => {
-  const maxSearchSize = 15; // limit of the search result size
+  //const maxSearchSize = 15; // limit of the search result size
 
-  const filtered = props.data
+  const filteredList = props.data
     .filter((item) => {
       return item.index.includes(props.input);
     })
-    .slice(0, maxSearchSize)
+    .slice(0)
     .map((item) => {
       return (
         <SearchResult
           index={item.index}
           key={item.index}
-          path={"images/" + item.index + ".png"}
+          path={"images/150/" + item.index + ".jpg"}
           clicked={() => props.clicked(item)}
         />
       );
     });
-  return <div className="SearchList">{filtered}</div>;
+  return (
+    <div className="SearchList">
+      <p>wyswietlono {filteredList.length} elementy</p>
+      {filteredList}
+    </div>
+  );
 };
 
 export default searchList;
